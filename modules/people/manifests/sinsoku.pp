3b0840934da03b28aacfd614bac119f8546e2a5a
sinsoku <sinsoku.listy@gmail.com>
2014-03-17 03:38:47 +0900

    add git, vim settings

--- modules/people/manifests/sinsoku.pp
+++ modules/people/manifests/sinsoku.pp
@@ -1,6 +1,10 @@
 class people::sinsoku {
   include redis
 
+  $home = "/Users/${::boxen_user}"
+  $dotfiles = "${home}/dotfiles"
+
+  repository { $dotfiles: source => 'sinsoku/dotfiles' }
   package {
     'vim':
       ensure => latest;
@@ -14,5 +18,44 @@ class people::sinsoku {
   exec { 'setup ricty':
     command => "cp -f ${homebrew::config::installdir}/share/fonts/Ricty*.ttf ~/Library/Fonts/ && fc-cache -vf",
     require => Package["ricty"]
+  }
+
+  # git
+  file { "${home}/.gitconfig":
+    ensure => link,
+    force => true,
+    target => "${dotfiles}/git/.gitconfig",
+    require => Repository[$dotfiles],
+  }
+
+  # vim
+  $vim_dir = "${home}/.vim"
+  $bundle_dir = "${vim_dir}/bundle"
+  $neobundle_dir = "${bundle_dir}/neobundle.vim"
+
+  file {
+    $vim_dir:
+      ensure => directory;
+    $bundle_dir:
+      ensure => directory;
+  }
+
+  file { "${home}/.vim/vimrc":
+    ensure => link,
+    force => true,
+    target => "${dotfiles}/vim/.vim/vimrc",
+    require => [
+      Repository[$dotfiles],
+      File[$vim_dir],
+    ]
+  }
+
+  repository { $neobundle_dir:
+    source => 'Shougo/neobundle.vim',
+    require => File[$bundle_dir]
+  }
+  exec { 'setup neobundle':
+    command => "${neobundle_dir}/bin/neoinstall",
+    require => Repository[$neobundle_dir],
   }
 }
