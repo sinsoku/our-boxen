2a61a85dcacedf92ee9cc7881347746bd087fe92
sinsoku <sinsoku.listy@gmail.com>
2014-03-17 00:31:05 +0900

    add ricty font

--- modules/people/manifests/sinsoku.pp
+++ modules/people/manifests/sinsoku.pp
@@ -1,3 +1,13 @@
 class people::sinsoku {
   include redis
+
+  # font: ricty
+  homebrew::tap { 'sanemat/font': }
+  package { 'ricty':
+    require => Homebrew::Tap["sanemat/font"]
+  }
+  exec { 'setup ricty':
+    command => "cp -f ${homebrew::config::installdir}/share/fonts/Ricty*.ttf ~/Library/Fonts/ && fc-cache -vf",
+    require => Package["ricty"]
+  }
 }
